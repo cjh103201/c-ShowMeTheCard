71420509057186025161e5b5c89d14f3cca068aa
cjh103201 <cjh103201@naver.com>
2017-06-15 23:43:58 +0900

    connet완료! select해보기-> 행의 개수 select, 전체 Member select하기

--- showmethecard/showmethecard/Controllers/AccountController.cs
+++ showmethecard/showmethecard/Controllers/AccountController.cs
@@ -5,52 +5,61 @@ using System.Web;
 using System.Web.Mvc;
 using System.Data.SqlTypes;
 using System.Data.SqlClient;
+using MySql.Data.MySqlClient;
 using System.Security.Cryptography;
 using showmethecard.Common;
+using showmethecard.Dal;
+using System;
+using System.Data;
 
 namespace showmethecard.Controllers
 {
     public class AccountController : Controller
     {
-
-        SqlConnection conn;
-        SqlCommand cmd;
+        MySqlConnection conn;
+        MySqlCommand cmd;
         
-
         // POST: Account Login
         [HttpPost]
         public ActionResult Login()
         {
-            string connStr = "Server=210.16.214.202;Database=rnh2;Uid=rnh2;Pwd=tjthsdbdlchl";
-            conn = new SqlConnection(connStr);
+            conn = new MySqlConnection(@"Data Source=210.16.214.202;Database=rnh2;User Id=rnh2;Password=tjthsdbdlchl");
             conn.Open();
+
             String mId = Request["mId"];
             String password = Request["password"];
-
             password = Util.SHA256Hash(password);
 
-            string sql = "SELECT M_ID mId, PASSWORD, M_NAME mName, EMAIL, PHONE, M_COUNT mCount, M_POINT mPoint, M_Level mLevel, USERTYPE, DELETED, REG_DATE regDate"
-                           + "FROM rnh2.MEMBER"
-                           + "WHERE M_ID = " + mId + "AND PASSWORD = " + password + "AND DELETED = '0'" ;
+            //select - ExecuteScalar
+            //            cmd = new MySqlCommand();
+            //            cmd.Connection = conn;
+            //            cmd.CommandText = "SELECT count(*) FROM rnh2.MEMBER WHERE M_ID = " + mId + "AND PASSWORD = " + password + "AND DELETED = '0'" ;
+            //            object result = cmd.ExecuteScalar();
 
-            cmd = new SqlCommand(sql, conn);
-            SqlDataReader dr = cmd.ExecuteReader();
+            //            Console.WriteLine("records : "+result);
 
-            if(dr != null)
-            {
-                while (dr.Read())
-                {
-                    ViewBag.Message = dr["mName"];
-                }
-                dr.Close();
+            //select - dataTable
+            //            string sql = "SELECT M_ID mId, PASSWORD, M_NAME mName, EMAIL, PHONE, M_COUNT mCount, M_POINT mPoint, M_Level mLevel, USERTYPE, DELETED, REG_DATE regDate"
+            //                       + "FROM rnh2.MEMBER"
+            //                       + "WHERE M_ID = " + mId + "AND PASSWORD = " + password + "AND DELETED = '0'";
+            //            DataSet ds = new DataSet();
+            //            MySqlDataAdapter da = new MySqlDataAdapter(sql, conn);
+            //            da.Fill(ds, "member");
 
-                return RedirectToRoute(new { Controller = "Home", action = "About" });
+            //            DataTable dt = ds.Tables["member"];
+            //            foreach(DataRow dr in dt.Rows)
+            //            {
+            //                Console.WriteLine(string.Format("mId={0}, password={1}, dr[mname], dr[mLevel]"));
+            //            }
 
-            } else
-            {
-                return RedirectToRoute(new { Controller = "Home", Action = "Index" });
-            }
+            //행의 개수 select
+            cmd = new MySqlCommand();
+            String sql = "SELECT Count(*) From rnh2.MEMBER";
 
+
+
+            return RedirectToRoute(new { Controller = "Home", action = "About" });
+            
         }
     }
 }
---
+++ showmethecard/showmethecard/Models/Member.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace showmethecard.Models
+{
+    public class Member
+    {
+        public String mId { get; set; }
+        public String password { get; set; }
+        public String mName { get; set; }
+        public String email { get; set; }
+        public String phone { get; set; }
+        public int mCount { get; set; }
+        public int mPoint { get; set; }
+        public String userType { get; set; }
+        public bool deleted { get; set; }
+        public DateTime regDate { get; set; }
+        public int mLevel { get; set; }
+
+
+    }
+}
