7e230919349fbf15b3cd482df0f4bb94622bf05b
cjh103201 <cjh103201@naver.com>
2017-06-15 18:59:49 +0900

    로그인 구현 작업 중, 디비 연결 안됨! 확인하고 수정할 것!!

---
+++ showmethecard/showmethecard/Common/Util.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace showmethecard.Common
+{
+    public class Util
+    {
+        //SHA256 256bit 암호화
+        public static string SHA256Hash(string Data)
+        {
+            SHA256 sha = new SHA256Managed();
+            byte[] hash = sha.ComputeHash(Encoding.ASCII.GetBytes(Data));
+
+            StringBuilder sb = new StringBuilder();
+            foreach(byte b in hash)
+            {
+                sb.AppendFormat("{0:x2}", b);
+            }
+
+            return sb.ToString();
+        }
+    }
+}
--- showmethecard/showmethecard/Controllers/AccountController.cs
+++ showmethecard/showmethecard/Controllers/AccountController.cs
@@ -3,15 +3,54 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data.SqlTypes;
+using System.Data.SqlClient;
+using System.Security.Cryptography;
+using showmethecard.Common;
 
 namespace showmethecard.Controllers
 {
     public class AccountController : Controller
     {
-        // GET: Account
+
+        SqlConnection conn;
+        SqlCommand cmd;
+        
+
+        // POST: Account Login
+        [HttpPost]
         public ActionResult Login()
         {
-            return View();
+            string connStr = "Server=210.16.214.202;Database=rnh2;Uid=rnh2;Pwd=tjthsdbdlchl";
+            conn = new SqlConnection(connStr);
+            conn.Open();
+            String mId = Request["mId"];
+            String password = Request["password"];
+
+            password = Util.SHA256Hash(password);
+
+            string sql = "SELECT M_ID mId, PASSWORD, M_NAME mName, EMAIL, PHONE, M_COUNT mCount, M_POINT mPoint, M_Level mLevel, USERTYPE, DELETED, REG_DATE regDate"
+                           + "FROM rnh2.MEMBER"
+                           + "WHERE M_ID = " + mId + "AND PASSWORD = " + password + "AND DELETED = '0'" ;
+
+            cmd = new SqlCommand(sql, conn);
+            SqlDataReader dr = cmd.ExecuteReader();
+
+            if(dr != null)
+            {
+                while (dr.Read())
+                {
+                    ViewBag.Message = dr["mName"];
+                }
+                dr.Close();
+
+                return RedirectToRoute(new { Controller = "Home", action = "About" });
+
+            } else
+            {
+                return RedirectToRoute(new { Controller = "Home", Action = "Index" });
+            }
+
         }
     }
 }
